607dbdfeb26124b7ac9ccac31c255bae8898fdfe
DSlayerX666 <kongrenxi@gmail.com>
2024-09-01 22:58:05 +0800

    CCTV Indicator Now Only Appears When The Player Enters The Security Room

---
+++ City Heist/Assets/rxscene/Scripts/CCTVIndicator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CCTVIndicator : MonoBehaviour
+{
+    public GameObject[] cctvIndicators;
+
+    void Start()
+    {
+        foreach (var cctvIndicator in cctvIndicators)
+        {
+            cctvIndicator.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.jammedCCTV)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !GameManager.instance.jammedCCTV)
+        {
+            foreach (var cctvIndicator in cctvIndicators)
+            {
+                cctvIndicator.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && !GameManager.instance.jammedCCTV)
+        {
+            foreach (var cctvIndicator in cctvIndicators)
+            {
+                cctvIndicator.SetActive(false);
+            }
+        }
+    }
+}
