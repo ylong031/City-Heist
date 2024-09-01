b739e8f8809c1d002e386e9daffe444342f6cc56
DSlayerX666 <kongrenxi@gmail.com>
2024-09-01 18:46:41 +0800

    Fixed some bugs
    1. Fixed a bug with bullet holes not spawning on some walls.
    2. Press M key to view (enlarged) map.
    3. Fixed a bug with TextMeshPro text appearing black.
    4. Fixed minimap zoom in / out so that zooming doesn't affect room text.

--- City Heist/Assets/rxscene/Scripts/Minimaps.cs
+++ City Heist/Assets/rxscene/Scripts/Minimaps.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 public class Minimaps : MonoBehaviour
 {
     public GameObject[] minimaps;
+    Vector3 minimapOriginalPos;
+    Vector3 minimapOriginalScale;
+    bool isMaximized = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +18,44 @@ public class Minimaps : MonoBehaviour
                 minimap.SetActive(false);
             }
         }
+        else
+        {
+            minimapOriginalPos = minimaps[1].transform.position;
+            minimapOriginalScale = minimaps[1].transform.localScale;
+        }
     }
 
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "CityScene")
+        if (SceneManager.GetActiveScene().name == "CityScene" || PlayerPrefs.GetInt("EnableMinimap", 1) == 0)
         {
             return;
         }
 
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (!isMaximized)
+            {
+                isMaximized = true;
+                minimaps[1].transform.localPosition = new Vector3(250f, 226f, 0f);
+                minimaps[1].transform.localScale = new Vector3(1f, 1f, minimapOriginalScale.z);
+            }
+            else if (isMaximized)
+            {
+                isMaximized = false;
+                minimaps[1].transform.position = minimapOriginalPos;
+                minimaps[1].transform.localScale = minimapOriginalScale;
+            }
+        }
+
         if (SceneManager.GetActiveScene().name == "Bank 1")
         {
-            // If Zoomed In Enough, Minimap Will Follow Player
+/*            if (isMaximized)
+            {
+                return;
+            }*/
+
+            // If zoomed in enough, minimap will follow the player
             if (minimaps[0].GetComponent<Camera>().orthographicSize <= 19.45f)
             {
                 minimaps[0].transform.position = new Vector3(GameManager.instance.playerMovement.transform.position.x, minimaps[0].transform.position.y, GameManager.instance.playerMovement.transform.position.z);
@@ -42,6 +72,8 @@ public class Minimaps : MonoBehaviour
                 // Zoom Out
                 minimaps[0].GetComponent<Camera>().orthographicSize += 2f;
                 minimaps[0].transform.position = new Vector3(minimaps[0].transform.position.x + (-3.7f - GameManager.instance.playerMovement.transform.position.x) / 5, minimaps[0].transform.position.y, minimaps[0].transform.position.z + (-22.22f - GameManager.instance.playerMovement.transform.position.z) / 5);
+
+                // When zoomed out to maximum, reset camera position
                 if (minimaps[0].GetComponent<Camera>().orthographicSize == 25.45f)
                 {
                     minimaps[0].transform.position = new Vector3(-3.7f, minimaps[0].transform.position.y, -22.22f);
@@ -50,7 +82,12 @@ public class Minimaps : MonoBehaviour
         }
         else if (SceneManager.GetActiveScene().name == "Bank 2")
         {
-            // If Zoomed In Enough, Minimap Will Follow Player
+/*            if (isMaximized)
+            {
+                return;
+            }*/
+
+            // If zoomed in enough, minimap will follow the player
             if (minimaps[0].GetComponent<Camera>().orthographicSize <= 23.8f)
             {
                 minimaps[0].transform.position = new Vector3(GameManager.instance.playerMovement.transform.position.x, minimaps[0].transform.position.y, GameManager.instance.playerMovement.transform.position.z);
@@ -67,6 +104,8 @@ public class Minimaps : MonoBehaviour
                 // Zoom Out
                 minimaps[0].GetComponent<Camera>().orthographicSize += 2f;
                 minimaps[0].transform.position = new Vector3(minimaps[0].transform.position.x + (-9.1f - GameManager.instance.playerMovement.transform.position.x) / 5, minimaps[0].transform.position.y, minimaps[0].transform.position.z + (-19.64f - GameManager.instance.playerMovement.transform.position.z) / 5);
+
+                // When zoomed out to maximum, reset camera position
                 if (minimaps[0].GetComponent<Camera>().orthographicSize == 29.8f)
                 {
                     minimaps[0].transform.position = new Vector3(-9.1f, minimaps[0].transform.position.y, -19.64f);
