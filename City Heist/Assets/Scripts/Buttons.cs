f05fd3a08a447e24a5ce0489e5c75c3ec0184515
Ricole <www.yiru@hotmail.com>
2024-09-05 00:04:10 +0800

    implement button sound in the Buttons Script
    1. Coded function to trigger button sound
    2. Added button sound source to the main menu so that the sound will play when buttons in the main menu are pressed.

--- City Heist/Assets/Scripts/Buttons.cs
+++ City Heist/Assets/Scripts/Buttons.cs
@@ -9,6 +9,10 @@ public class Buttons : MonoBehaviour
     public TMP_Dropdown qualityDropdown;
     public GameObject rulesPanel;
 
+    //add reference to the AudioSource and AudioClip
+    public AudioSource buttonAudioSource;
+    public AudioClip buttonSound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,31 +28,55 @@ public class Buttons : MonoBehaviour
         PlayerPrefs.SetInt("EnableMinimap", 1);
     }
 
+    //create function to play the sound
+    private void PlayButtonSound()
+    {
+        if (buttonAudioSource != null && buttonSound != null)
+        {
+            buttonAudioSource.PlayOneShot(buttonSound);
+        }
+    }
+
     public void Play() 
     {
+        //play the button sound when button pressed
+        PlayButtonSound();
+
         //SceneManager.LoadScene(1);
         //StartCoroutine(SceneTransition.instance.TransitionToScene("CityScene"));
         rulesPanel.SetActive(true);
     }
     public void Exit() 
     {
+        //play the button sound when button pressed
+        PlayButtonSound();
+
         Debug.Log("Quitting......");
         Application.Quit();
     }
 
     public void Rules() 
     {
+        //play the button sound when button pressed
+        PlayButtonSound();
+
         Debug.Log("Rules!");
     }
 
     public void SetQuality(int qualityIndex)
     {
+        //play the button sound when button pressed
+        PlayButtonSound();
+
         QualitySettings.SetQualityLevel(qualityIndex);
         //Debug.Log(QualitySettings.GetQualityLevel());
     }
 
     public void EnableMinimap(bool b)
     {
+        //play the button sound when button pressed
+        PlayButtonSound();
+
         Debug.Log(b);
         if (b)
         {
