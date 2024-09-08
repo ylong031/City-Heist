1b6b66b120091e69f8ec1f16e929a0c6d673460a
ylong031 <140056328+ylong031@users.noreply.github.com>
2024-09-08 15:39:20 +0800

    remove unused assets
    remove unused assets

---
+++ City Heist/Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public int level;
+  
+    private int currentlevel=1;
+
+    Camera minicamera;
+   
+   
+
+    [SerializeField] LayerMask mask1;
+    [SerializeField] LayerMask mask2;
+
+
+    [SerializeField] GameObject lvl1text;
+    [SerializeField] GameObject lvl2text;
+    private void Awake()
+    {
+        minicamera = GetComponent<Camera>();
+        
+    }
+
+
+    void Update()
+    {
+        if(level != currentlevel)
+        {
+            if(level== 1)
+            {
+                minicamera.cullingMask = mask1;
+                lvl1text.SetActive(true);
+                lvl2text.SetActive(false);
+
+            }
+            else 
+            {
+                minicamera.cullingMask = mask2;
+                lvl2text.SetActive(true);
+                lvl1text.SetActive(false);
+            }
+            currentlevel = level;
+        }
+                
+    }
+   
+}
